f52e3d82f0e47a9083f6cdcf3419fc8753a222c2
AmpFkj <ampfkj@gmail.com>
2025-05-09 08:16:38 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
-if getgetnv().MainKey == "nil" then
+if getgenv().MainKey == "nil" then
 getgenv().MainLoad = true
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AmpFkj/Project/refs/heads/main/mm2.lua"))() 
 end
